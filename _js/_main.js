eb50441664cb25715c585f10f788f5e09edd8d92
Paul Catlin <paul@ecatlin.com>
2025-05-09 11:55:11 +0100

    headers

---
+++ _js/_main.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const copyrightElement = document.querySelector('.copyright')
+    if (copyrightElement) {
+        const currentYear = new Date().getFullYear()
+        copyrightElement.innerHTML = `&copy; ${currentYear} Tick It`
+    }
+})
